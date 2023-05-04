bc999be51c0c3a9bf37fc60e2eec8b830cf55567
EC2 Default User <129925697+Jack-00000@users.noreply.github.com>
2023-05-04 05:10:23 +0000

    second

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
  
-  
+  devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about"
-  devise_for :users
+
   
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
+  
+  resource :favorites, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
 end
